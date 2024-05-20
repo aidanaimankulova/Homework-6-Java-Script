7ece99b4208269062b418f3eb400076192644e74
aidanaimankulova <164762642+aidanaimankulova@users.noreply.github.com>
2024-05-20 13:45:01 +0200

    Add files via upload

---
+++ Homework6/script/script.js
@@ -0,0 +1,33 @@
+// Work1
+// let number = +prompt("Enter the number")
+
+// console.log(number**3);
+
+
+// Work2
+// let firstNumber = Number(prompt("Enter first number"));
+// let secondNumber = Number(prompt("Enter second number"));
+
+// console.log(Number(firstNumber));
+// console.log(Number(secondNumber));
+
+// let sum = firstNumber + secondNumber;
+
+// console.log(sum);
+
+
+// Work3
+let user_name = prompt("UserName");
+let user_surname = prompt("UserSurname");
+let user_age = prompt("UserAge");
+let user_position = prompt("UserPosition");
+
+let thisYear = 2024;
+let birthYear = thisYear - user_age;
+
+console.log(`Hi, my name is ${user_name} ${user_surname}.I war born in ${birthYear}.
+I'm a ${user_position}.`);
+
+
+
+
